08a94c28f4d5ae7ad4a6d9f6044b8508114665cc
samiulsayhan <samiulsayhan@gmail.com>
2026-01-26 20:09:08 +0600

    new next.js project MY-app

---
+++ my-app/src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+    return(
+        <div>
+            <h1>This is contact page components</h1>
+        </div>
+    )
+}
---
+++ my-app/src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+    return(
+        <div>
+            <h1>This is contact page components</h1>
+        </div>
+    )
+    
+}
