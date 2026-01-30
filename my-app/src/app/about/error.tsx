4866910b337213728ff8d98b727db18f9795d5d1
samiulsayhan <samiulsayhan@gmail.com>
2026-01-30 23:45:30 +0600

    ..

--- my-app/src/app/about/error.tsx
+++ my-app/src/app/about/error.tsx
@@ -1,8 +1,18 @@
 "use client";
-export default function AboutError(){
+
+import { useEffect } from "react";
+
+export default function AboutError({error,reset}:{error:Error & {digest?:string}; reset: ()=>void}){
+
+    useEffect(()=>{
+        // we can pass this error to a logger
+        console.error(error)
+    })
+
     return(
         <div>
             <h1>there is a error in this page</h1>
+            <button onClick={()=>reset()}>Retry</button>
         </div>
     )
 }
