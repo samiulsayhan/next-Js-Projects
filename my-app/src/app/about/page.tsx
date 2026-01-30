52070c79037be8d1e94036cc61ef4273ce59de0a
samiulsayhan <samiulsayhan@gmail.com>
2026-01-30 19:03:11 +0600

    ..

--- my-app/src/app/about/page.tsx
+++ my-app/src/app/about/page.tsx
@@ -1,7 +1,10 @@
+import { error } from "console"
 import { resolve } from "path"
 
 export default async function AboutPage() {
     await new Promise ((resolve)=>setTimeout(resolve,4000 ))
+
+    throw new Error ("Something went wrong")
     return(
         <div>
             <h1>This is contact page components</h1>
