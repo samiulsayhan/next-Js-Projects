35b690f43f54fb1524d408a0c953682e7d95a60e
samiulsayhan <samiulsayhan@gmail.com>
2026-01-31 22:09:30 +0600

    ..

--- my-app/src/app/about/page.tsx
+++ my-app/src/app/about/page.tsx
@@ -1,9 +1,11 @@
 import { error } from "console"
 import { resolve } from "path"
 
+export const dynamic = "force-dynamic";
+
 export default async function AboutPage() {
     // for simulating load time 
-    // await new Promise ((resolve)=>setTimeout(resolve,4000 ))
+    await new Promise ((resolve)=>setTimeout(resolve,4000 ))
 
     // for simalating error
     // throw new Error ("Something went wrong")
---
+++ my-app/src/app/dashboard/layout.tsx
@@ -0,0 +1,8 @@
+export default function DashboardLayout({children}){
+    return(
+        <div>
+            <h1>This is the dashboard layout</h1>
+            {children}
+        </div>
+    )
+}
