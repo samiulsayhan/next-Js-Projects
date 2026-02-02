05c9bc40043c1f4f0bd4d5a77168a0177cb06580
samiulsayhan <samiulsayhan@gmail.com>
2026-02-02 17:29:43 +0600

    ..

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components/layout/navbar";
 import { ThemeProvider } from "@/provider/ThemeProvider";
 
 const geistSans = Geist({
@@ -35,7 +34,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {/* .. */}
-          <Navbar/>
+          
           {children}
         </ThemeProvider>
 
