4f8f08135efecea6b6ed884ef4bf63388bf43d82
samiulsayhan <samiulsayhan@gmail.com>
2026-02-02 15:49:19 +0600

    ..

--- my-app/src/app/dashboard/layout.tsx
+++ my-app/src/app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-export default function DashboardLayout({children}){
+export default function DashboardLayout({children}:{children:React.ReactNode}){
     return(
         <div>
             <h1>This is the dashboard layout</h1>
